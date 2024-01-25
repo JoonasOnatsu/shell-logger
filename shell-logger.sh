42d435207b124c354d484f926c7a16fdfcf2cca4
Joonas Onatsu <joonas.onatsu@unikie.com>
2024-01-26 01:18:06 +0200

    WIP: refactor logger

--- shell-logger.sh
+++ shell-logger.sh
@@ -1,44 +1,50 @@
 #!/usr/bin/env bash
 
-## Description {{{
-#
-# Logger for shell script.
-#
-# Homepage: https://github.com/rcmdnk/shell-logger
-#
+# ==============================================================================
+# Logger for shell scripts.
+# Based on 'shell-logger' project by rcmdnk (https://github.com/rcmdnk/shell-logger).
+# ==============================================================================
+
+# cSpell: disable
+# shellcheck shell=bash
+# shellcheck source=/dev/null
+# shellcheck disable=SC2317,SC2312,SC2155
+
 _LOGGER_NAME="shell-logger"
 _LOGGER_VERSION="v0.2.0"
 _LOGGER_DATE="04/Feb/2019"
-# }}}
 
-## License {{{
+# MIT License
 #
-#MIT License
+# Copyright (c) 2017 rcmdnk
 #
-#Copyright (c) 2017 rcmdnk
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 #
-#Permission is hereby granted, free of charge, to any person obtaining a copy
-#of this software and associated documentation files (the "Software"), to deal
-#in the Software without restriction, including without limitation the rights
-#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#copies of the Software, and to permit persons to whom the Software is
-#furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
 #
-#The above copyright notice and this permission notice shall be included in all
-#copies or substantial portions of the Software.
-#
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#SOFTWARE.
-# }}}
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
-# Default variables {{{
+# Default variables.
 LOGGER_DATE_FORMAT=${LOGGER_DATE_FORMAT:-'%Y/%m/%d %H:%M:%S'}
-LOGGER_LEVEL=${LOGGER_LEVEL:-1} # 0: debug, 1: info, 2: notice, 3: warning, 4: error
+
+# 0 -> debug
+# 1 -> info
+# 2 -> notice
+# 3 -> warning
+# 4 -> error
+LOGGER_LEVEL=${LOGGER_LEVEL:-1}
 LOGGER_STDERR_LEVEL=${LOGGER_STDERR_LEVEL:-4}
 LOGGER_DEBUG_COLOR=${LOGGER_DEBUG_COLOR:-"3"}
 LOGGER_INFO_COLOR=${LOGGER_INFO_COLOR:-""}
@@ -46,213 +52,224 @@ LOGGER_NOTICE_COLOR=${LOGGER_NOTICE_COLOR:-"36"}
 LOGGER_WARNING_COLOR=${LOGGER_WARNING_COLOR:-"33"}
 LOGGER_ERROR_COLOR=${LOGGER_ERROR_COLOR:-"31"}
 LOGGER_COLOR=${LOGGER_COLOR:-auto}
-LOGGER_COLORS=("$LOGGER_DEBUG_COLOR" "$LOGGER_INFO_COLOR" "$LOGGER_NOTICE_COLOR" "$LOGGER_WARNING_COLOR" "$LOGGER_ERROR_COLOR")
-if [ "${LOGGER_LEVELS:-}" = "" ];then
+LOGGER_COLORS=("${LOGGER_DEBUG_COLOR}" "${LOGGER_INFO_COLOR}" "${LOGGER_NOTICE_COLOR}" "${LOGGER_WARNING_COLOR}" "${LOGGER_ERROR_COLOR}")
+if [[ ${LOGGER_LEVELS-} == "" ]]; then
   LOGGER_LEVELS=("DEBUG" "INFO" "NOTICE" "WARNING" "ERROR")
 fi
-LOGGER_SHOW_TIME=${LOGGER_SHOW_TIME:-1}
-LOGGER_SHOW_FILE=${LOGGER_SHOW_FILE:-1}
-LOGGER_SHOW_LEVEL=${LOGGER_SHOW_LEVEL:-1}
+LOGGER_SHOW_TIME=${LOGGER_SHOW_TIME:-true}
+LOGGER_SHOW_FILE=${LOGGER_SHOW_FILE:-true}
+LOGGER_SHOW_LEVEL=${LOGGER_SHOW_LEVEL:-true}
 LOGGER_ERROR_RETURN_CODE=${LOGGER_ERROR_RETURN_CODE:-100}
-LOGGER_ERROR_TRACE=${LOGGER_ERROR_TRACE:-1}
-# }}}
+LOGGER_ERROR_TRACE=${LOGGER_ERROR_TRACE:-true}
 
-# Other global variables {{{
+# Other global variables.
 _LOGGER_WRAP=0
-#}}}
 
-# Functions {{{
-_logger_version () {
-  printf "%s %s %s\\n" "$_LOGGER_NAME" "$_LOGGER_VERSION" "$_LOGGER_DATE"
+# Functions.
+function _logger_version() {
+  printf '%s %s %s\n' "${_LOGGER_NAME}" "${_LOGGER_VERSION}" "${_LOGGER_DATE}"
 }
 
-_get_level () {
-  if [ $# -eq 0 ];then
-    local level=1
-  else
-    local level=$1
-  fi
-  if ! expr "$level" : '[0-9]*' >/dev/null;then
-    [ -z "${ZSH_VERSION:-}" ] || emulate -L ksh
-    local i=0
-    while [ $i -lt ${#LOGGER_LEVELS[@]} ];do
-      if [ "$level" = "${LOGGER_LEVELS[$i]}" ];then
-        level=$i
+function _get_curr_logger_level() {
+  local level=${1:-1}
+
+  # Validate log level setting.
+  if ! expr "${level}" : '[0-9]*' > /dev/null; then
+    [[ -n ${ZSH_VERSION-} ]] && emulate -L ksh
+
+    local idx=0
+    while [[ ${idx} -lt ${#LOGGER_LEVELS[@]} ]]; do
+      if [[ ${level} == "${LOGGER_LEVELS[${idx}]}" ]]; then
+        level=${idx}
         break
       fi
-      ((i++))
+      ((idx++))
     done
   fi
-  echo $level
+
+  printf '%s' "${level}"
 }
 
-_logger_level () {
-  [ "$LOGGER_SHOW_LEVEL" -ne 1 ] && return
-  if [ $# -eq 1 ];then
-    local level=$1
-  else
-    local level=1
-  fi
-  [ -z "${ZSH_VERSION:-}" ] || emulate -L ksh
-  printf "[${LOGGER_LEVELS[$level]}]"
+function _logger_level_str() {
+  [[ ${LOGGER_SHOW_LEVEL:-false} == false ]] && return
+  local level=${1:-1}
+
+  [[ -n ${ZSH_VERSION-} ]] && emulate -L ksh
+  printf '[%s]' "${LOGGER_LEVELS[${level}]}"
 }
 
-_logger_time () {
-  [ "$LOGGER_SHOW_TIME" -ne 1 ] && return
-  printf "[$(date +"$LOGGER_DATE_FORMAT")]"
+function _logger_time_str() {
+  [[ ${LOGGER_SHOW_TIME:-false} == false ]] && return
+  printf '[%s]' "$(date +"${LOGGER_DATE_FORMAT}")"
 }
 
-_logger_file () {
-  [ "$LOGGER_SHOW_FILE" -ne 1 ] && return
-  local i=0
-  if [ $# -ne 0 ];then
-    i=$1
-  fi
-  if [ -n "$BASH_VERSION" ];then
-    printf "[${BASH_SOURCE[$((i+1))]}:${BASH_LINENO[$i]}]"
-  else
+function _logger_file_str() {
+  [[ ${LOGGER_SHOW_FILE:-false} == false ]] && return
+  local idx=${1:-0}
+
+  if [[ -n ${BASH_VERSION} ]]; then
+    printf '[%s]' "${BASH_SOURCE[$((idx + 1))]}:${BASH_LINENO[${idx}]}"
+  elif [[ -n ${ZSH_VERSION-} ]]; then
     emulate -L ksh
-    printf "[${funcfiletrace[$i]}]"
+    printf '[%s]' "${funcfiletrace[${idx}]}"
   fi
 }
 
-_logger () {
+function _logger() {
+
   ((_LOGGER_WRAP++)) || true
   local wrap=${_LOGGER_WRAP}
   _LOGGER_WRAP=0
-  if [ $# -eq 0 ];then
-    return
-  fi
-  local level="$1"
+
+  [[ $# -eq 0 ]] && return
+  [[ $1 -lt "$(_get_curr_logger_level "${LOGGER_LEVEL}")" ]] && return
+
+  local level=$1
   shift
-  if [ "$level" -lt "$(_get_level "$LOGGER_LEVEL")" ];then
-    return
+
+  local msg_prefix="$(_logger_time_str)$(_logger_file_str "${wrap}")$(_logger_level_str "${level}")"
+
+  # Add prefix with a space only if prefix not is empty.
+  local msg="${msg_prefix:+${msg_prefix} }$*"
+
+  # Escape $ is msg to be able to use eval below without trying to resolve a variable.
+  msg="${msg/\$/\\\$}"
+
+  local logger_print=printf
+  local logger_outfile=1
+
+  # Output to stderr also after certain level.
+  if [[ ${level} -ge ${LOGGER_STDERR_LEVEL} ]]; then
+    logger_outfile=2
+    logger_print=">&2 printf"
   fi
-  local msg_prefix="$(_logger_time)$(_logger_file "$wrap")$(_logger_level "$level")"
-  local msg="${msg_prefix:+$msg_prefix }$*" # add prefix with a space only if prefix not is empty
-  msg="${msg/\$/\\\$}" # escape $ is msg to be able to use eval below without trying to resolve a variable
-  local _logger_printf=printf
-  local out=1
-  if [ "$level" -ge "$LOGGER_STDERR_LEVEL" ];then
-    out=2
-    _logger_printf=">&2 printf"
-  fi
-  if [ "$LOGGER_COLOR" = "always" ] || { test "$LOGGER_COLOR" = "auto"  && test  -t $out ; };then
-    [ -z "${ZSH_VERSION:-}" ] || emulate -L ksh
-    eval "$_logger_printf \"\\e[${LOGGER_COLORS[$level]}m%s\\e[m\\n\"  \"$msg\""
+
+  if [[ ${LOGGER_COLOR} == "always" ]] || { [[ ${LOGGER_COLOR} == "auto" ]] && [[ -t ${logger_outfile} ]]; }; then
+    [[ -n ${ZSH_VERSION-} ]] && emulate -L ksh
+    eval "${logger_print} \"\\e[${LOGGER_COLORS[${level}]}m%s\\e[m\\n\"  \"${msg}\""
   else
-    eval "$_logger_printf \"%s\\n\" \"$msg\""
+    eval "${logger_print} \"%s\\n\" \"${msg}\""
   fi
 }
 
-debug () {
+function debug() {
   ((_LOGGER_WRAP++)) || true
   _logger 0 "$*"
 }
 
-information () {
+function information() {
   ((_LOGGER_WRAP++)) || true
   _logger 1 "$*"
 }
-info () {
+function info() {
   ((_LOGGER_WRAP++)) || true
   information "$*"
 }
 
-notification () {
+function notification() {
   ((_LOGGER_WRAP++)) || true
   _logger 2 "$*"
 }
-notice () {
+function notice() {
   ((_LOGGER_WRAP++)) || true
   notification "$*"
 }
 
-warning () {
+function warning() {
   ((_LOGGER_WRAP++)) || true
   _logger 3 "$*"
 }
-warn () {
+function warn() {
   ((_LOGGER_WRAP++)) || true
   warning "$*"
 }
 
-error () {
+function error() {
   ((_LOGGER_WRAP++)) || true
-  if [ "$LOGGER_ERROR_TRACE" -eq 1 ];then
+
+  if [[ ${LOGGER_ERROR_TRACE:-false} == true ]]; then
     {
-      [ -z "${ZSH_VERSION:-}" ] || emulate -L ksh
       local first=0
-      if [ -n "$BASH_VERSION" ];then
-        local current_source=$(echo "${BASH_SOURCE[0]##*/}"|cut -d"." -f1)
+      if [[ -n ${BASH_VERSION} ]]; then
+        local current_source="$(printf '%s' "${BASH_SOURCE[0]##*/}" | cut -d"." -f1)"
         local func="${FUNCNAME[1]}"
-        local i=$((${#FUNCNAME[@]}-2))
-      else
-        local current_source=$(echo "${funcfiletrace[0]##*/}"|cut -d":" -f1|cut -d"." -f1)
+        local idx=$((${#FUNCNAME[@]} - 2))
+      elif [[ -n ${ZSH_VERSION-} ]]; then
+        emulate -L ksh
+        local current_source="$(printf '%s' "${funcfiletrace[0]##*/}" | cut -d":" -f1 | cut -d"." -f1)"
         local func="${funcstack[1]}"
-        local i=$((${#funcstack[@]}-1))
-        local last_source=${funcfiletrace[$i]%:*}
-        if [ "$last_source" = zsh ];then
-          ((i--))
-        fi
+        local idx=$((${#funcstack[@]} - 1))
+        local last_source="${funcfiletrace[${idx}]%:*}"
+        [[ ${last_source} == "zsh" ]] && ((idx--))
       fi
-      if [ "$current_source" = "shell-logger" ] && [ "$func" = err ];then
+
+      if [[ ${current_source} == "shell-logger" ]] && [[ ${func} == err ]]; then
         local first=1
       fi
-      if [ $i -ge $first ];then
-        echo "Traceback (most recent call last):"
+
+      if [[ ${idx} -ge ${first} ]]; then
+        printf 'Traceback (most recent call last):\n'
       fi
-      while [ $i -ge $first ];do
-        if [ -n "$BASH_VERSION" ];then
-          local file=${BASH_SOURCE[$((i+1))]}
-          local line=${BASH_LINENO[$i]}
+
+      while [[ ${idx} -ge ${first} ]]; do
+        if [[ -n ${BASH_VERSION} ]]; then
+          local file="${BASH_SOURCE[$((idx + 1))]}"
+          local line="${BASH_LINENO[${idx}]}"
           local func=""
-          if [ ${BASH_LINENO[$((i+1))]} -ne 0 ];then
-            if [ "${FUNCNAME[$((i+1))]}" = "source" ];then
-              func=", in ${BASH_SOURCE[$((i+2))]}"
+
+          if [[ ${BASH_LINENO[$((idx + 1))]} -ne 0 ]]; then
+            if [[ ${FUNCNAME[$((idx + 1))]} == "source" ]]; then
+              func=", in ${BASH_SOURCE[$((idx + 2))]}"
             else
-              func=", in ${FUNCNAME[$((i+1))]}"
+              func=", in ${FUNCNAME[$((idx + 1))]}"
             fi
           fi
-          local func_call="${FUNCNAME[$i]}"
-          if [ "$func_call" = "source" ];then
-            func_call="${func_call} ${BASH_SOURCE[$i]}"
+
+          local func_call="${FUNCNAME[${idx}]}"
+          if [[ ${func_call} == "source" ]]; then
+            func_call="${func_call} ${BASH_SOURCE[${idx}]}"
           else
             func_call="${func_call}()"
           fi
-        else
-          local file=${funcfiletrace[$i]%:*}
-          local line=${funcfiletrace[$i]#*:}
+        elif [[ -n ${ZSH_VERSION-} ]]; then
+          emulate -L ksh
+          local file="${funcfiletrace[${idx}]%:*}"
+          local line="${funcfiletrace[${idx}]#*:}"
           local func=""
-          if [ -n "${funcstack[$((i+1))]}" ];then
-            if [ "${funcstack[$((i+1))]}" = "${funcfiletrace[$i]%:*}" ];then
-              func=", in ${funcfiletrace[$((i+1))]%:*}"
+
+          if [[ -n ${funcstack[$((idx + 1))]} ]]; then
+            if [[ ${funcstack[$((idx + 1))]} == "${funcfiletrace[${idx}]%:*}" ]]; then
+              func=", in ${funcfiletrace[$((idx + 1))]%:*}"
             else
-              func=", in ${funcstack[$((i+1))]}"
+              func=", in ${funcstack[$((idx + 1))]}"
             fi
           fi
-          local func_call="${funcstack[$i]}"
-          if [ "$func_call" = "${funcfiletrace[$((i-1))]%:*}" ];then
-            func_call="source ${funcfiletrace[$((i-1))]%:*}"
+
+          local func_call="${funcstack[${idx}]}"
+          if [[ ${func_call} == "${funcfiletrace[$((idx - 1))]%:*}" ]]; then
+            func_call="source ${funcfiletrace[$((idx - 1))]%:*}"
           else
             func_call="${func_call}()"
           fi
         fi
-        echo "  File \"${file}\", line ${line}${func}"
-        if [ $i -gt $first ];then
-          echo "    $func_call"
+
+        printf '  File \"%s\", line %s%s\n' "${file}" "${line}" "${func}"
+
+        if [[ ${idx} -gt ${first} ]]; then
+          printf '    %s\n' "${func_call}"
         else
-          echo ""
+          printf '\n'
         fi
-        ((i--))
+
+        ((idx--))
       done
     } 1>&2
   fi
   _logger 4 "$*"
-  return "$LOGGER_ERROR_RETURN_CODE"
+
+  return "${LOGGER_ERROR_RETURN_CODE}"
 }
-err () {
+function err() {
   ((_LOGGER_WRAP++)) || true
   error "$*"
 }
-# }}}
